7d8676a7f52d1e7196aede250dca4554cf540097
tytouan <150046405+tytypou@users.noreply.github.com>
2025-03-19 11:21:34 +0100

    Add files via upload

---
+++ app.py.py
@@ -0,0 +1,11 @@
+import streamlit as st
+#création page présentation
+
+def main():
+    st.title("Essayez de trouver ce que ça veut dire !")
+    st.markdown("ce jeu vous permettra peut-être de trouver le trésor !")
+    
+
+if __name__ == '__main__':
+    main() # Utile si vous voulez tester la page en standalone
+
